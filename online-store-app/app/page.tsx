eda5f70890ef14cad987433fd334b884f64292c6
atcherdsd <94963661+atcherdsd@users.noreply.github.com>
2023-06-28 18:06:07 +0300

    feat: change before go away

--- online-store-app/app/page.tsx
+++ online-store-app/app/page.tsx
@@ -1,67 +1,77 @@
-import React, { useEffect, useState } from 'react';
-import { GetServerSideProps, NextPage } from 'next';
-import Header from '../components/Header';
-import { GoodsData, Links, Positions } from '../types/types';
-import Search from '../components/Search';
-import { StorageItems } from '../types/enums';
+// import React, { useEffect, useState } from 'react';
+// import { GetServerSideProps, NextPage } from 'next';
+// import Header from '../components/Header';
+// import { Links, Positions } from '../types/types';
+// import Search from '../components/Search';
+// import { StorageItems } from '../types/enums';
 import './globals.css';
+// import goodsData from '../data/goods.json';
 
-export interface PropsTypes {
-  storeName: string;
-  links: Links;
-  goodsData: GoodsData;
-}
+// export interface PropsTypes {
+//   storeName: string;
+//   links: Links;
+//   goodsData: GoodsData;
+// }
 
-const Main: NextPage<{ storeName: string; links: Links; goodsData: GoodsData }> = ({
-  storeName,
-  links,
-  goodsData,
-}: PropsTypes): React.JSX.Element => {
-  const [searchedValue, setSearchedValue] = useState(
-    (localStorage.getItem(StorageItems.SearchedValue) as string) || ''
-  );
+// const Main: NextPage<{ storeName: string; links: Links; goodsData: GoodsData }> = ({
+//   storeName,
+//   links,
+//   goodsData,
+// }: PropsTypes): React.JSX.Element => {
+const Home = () => {
+  // const storeName = 'smartphone universe';
+  // const links: Links = {
+  //   main: '/',
+  //   about: '/about',
+  // };
+  // const goodsData: GoodsData = await import('../data/goods.json', { assert: { type: 'json' } });
 
-  function searchText(event: { target: { value: string } }): void {
-    setSearchedValue(event.target.value);
-  }
+  // const [searchedValue, setSearchedValue] = useState(
+  //   (localStorage.getItem(StorageItems.SearchedValue) as string) || ''
+  // );
 
-  const dataSearch: Positions[] = goodsData.goods.filter(
-    (item: Positions): boolean =>
-      item.title.toString().toLowerCase().includes(searchedValue.toString().toLowerCase()) ||
-      item.description.info
-        .toString()
-        .toLowerCase()
-        .includes(searchedValue.toString().toLowerCase())
-  );
+  // function searchText(event: { target: { value: string } }): void {
+  //   setSearchedValue(event.target.value);
+  // }
 
-  function setLocalStorage(): void {
-    localStorage.setItem(StorageItems.SearchedValue, searchedValue);
-  }
-  useEffect(setLocalStorage, [searchedValue]);
+  // const dataSearch: Positions[] = goodsData.goods.filter(
+  //   (item: Positions): boolean =>
+  //     item.title.toString().toLowerCase().includes(searchedValue.toString().toLowerCase()) ||
+  //     item.description.info
+  //       .toString()
+  //       .toLowerCase()
+  //       .includes(searchedValue.toString().toLowerCase())
+  // );
+
+  // function setLocalStorage(): void {
+  //   localStorage.setItem(StorageItems.SearchedValue, searchedValue);
+  // }
+  // useEffect(setLocalStorage, [searchedValue]);
 
   return (
     <>
-      <Header storeName={storeName} links={links} />
-      <Search searchedValue={searchedValue} searchText={searchText} dataSearch={dataSearch} />
+      <div>Hello</div>
+      {/* <Header storeName={storeName} links={links} />
+      <Search searchedValue={searchedValue} searchText={searchText} dataSearch={dataSearch} /> */}
     </>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const storeName = 'smartphone universe';
-  const links: Links = {
-    main: '/',
-    about: '/about',
-  };
-  const goodsData: GoodsData = await import('../data/goods.json', { assert: { type: 'json' } });
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const storeName = 'smartphone universe';
+//   const links: Links = {
+//     main: '/',
+//     about: '/about',
+//   };
+//   const goodsData: GoodsData = await import('../data/goods.json', { assert: { type: 'json' } });
 
-  return {
-    props: {
-      storeName,
-      links,
-      goodsData,
-    },
-  };
-};
+//   return {
+//     props: {
+//       storeName,
+//       links,
+//       goodsData,
+//     },
+//   };
+// };
 
-export default Main;
+export default Home;
